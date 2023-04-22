8feeec773f6729fd8a9dd84edbc8d14bcc059674
DeborahValerio <dborah.v@gmail.com>
2023-04-22 16:36:00 -0300

    Student insert

--- src/main/java/com/debproject/onlineCourses/resources/StudentResource.java
+++ src/main/java/com/debproject/onlineCourses/resources/StudentResource.java
@@ -1,13 +1,17 @@
 package com.debproject.onlineCourses.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.debproject.onlineCourses.entities.Student;
 import com.debproject.onlineCourses.services.StudentService;
@@ -31,5 +35,10 @@ public class StudentResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Student> insert(@RequestBody Student obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
