43298c74c38a7b162ebf249822ff9de7d871bcff
DeborahValerio <dborah.v@gmail.com>
2023-04-22 14:08:32 -0300

    Payment enum

---
+++ src/main/java/com/debproject/onlineCourses/entities/enums/Payment.java
@@ -0,0 +1,29 @@
+package com.debproject.onlineCourses.entities.enums;
+
+public enum Payment {
+
+	PIX(1),
+	DEBITO(2),
+	CREDITO_AVISTA(3),
+	CREDITO_APRAZO(4);
+
+	private int code;
+	
+	private Payment(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public static Payment valueOf(int code) {
+		for(Payment value : Payment.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+	
+}
